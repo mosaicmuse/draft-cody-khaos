3d73c309d3fc77a306666adbc79d0f86aae6c64a
mosaicmuse <93966161+mosaicmuse@users.noreply.github.com>
2022-11-18 14:43:54 -0800

    Add hamburger, start responsive styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,8 @@
-$(function () {
-  $(".img-w").each(function () {
-    $(this).wrap("<div class='img-c'></div>");
-    let imgSrc = $(this).find("img").attr("src");
-    $(this).css("background-image", "url(" + imgSrc + ")");
-  });
-
-  $(".img-c").click(function () {
-    let w = $(this).outerWidth();
-    let h = $(this).outerHeight();
-    let x = $(this).offset().left;
-    let y = $(this).offset().top;
-
-    $(".active").not($(this)).remove();
-    let copy = $(this).clone();
-    copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("active");
-    $(".active").css("top", y - 8);
-    $(".active").css("left", x - 8);
-
-    setTimeout(function () {
-      copy.addClass("positioned");
-    }, 0);
-  });
-});
-
-$(document).on("click", ".img-c.active", function () {
-  let copy = $(this);
-  copy.removeClass("positioned active").addClass("postactive");
-  setTimeout(function () {
-    copy.remove();
-  }, 500);
-});
+function toggleMenu() {
+  var x = document.getElementById("mobileMenu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
